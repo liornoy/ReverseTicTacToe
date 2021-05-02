ae49ab6e2c410b8962c578c278f61b1047bf4d67
Ofir Levy <ofirly@mta.ac.il>
2021-05-03 02:29:29 +0300

    Started to builf lagic layer

--- ReverseTicTacToe/Logic/Game.cs
+++ ReverseTicTacToe/Logic/Game.cs
@@ -6,28 +6,93 @@ using System.Threading.Tasks;
 
 namespace ReverseTicTacToe.Logic
 {
-    class Game
+    class RevereseTicTacToe
     {
-        private Player m_Player1, m_Player2;
-        private int m_CurrentTurn;
-        public int [][] GameBoard { get; set; }
-        public enum eGameTypes
+        public static enum eGameMode
         {
-            PVP = 1,
-            PVC = 2
+            PvP, PvAI,
         }
-        public Game(int i_BoardSize, eGameTypes i_GameType)
+        public enum eTurns
         {
-            m_CurrentTurn = 1;
-            if(i_GameType == eGameTypes.PVP)
+            Player1 = 1, Player2 = -1, 
+        }
+
+        //Ofir - Not so sure if these should be readonly, const or static?
+        private const int k_defaultBoardDimension = 3;
+        private const int k_GameBoardMinimumDimension = 3;
+        private const int k_GameBoardMaximumDimension = 9;
+        private const char k_defaultPlayer1Char = 'X';
+        private const char k_defaultPlayer2Char = 'O';
+        private const eGameMode k_defaultGameMode = eGameMode.PvP;
+        private const eTurns k_DefaultFirstTurn = eTurns.Player1;
+        private const defaultEmplySlotChar = ' ';
+
+        //members:
+        private char m_Player1Char;
+        private char m_Player2Char;
+        private char[,] m_GameBoard;
+        private int m_BoardDimension;
+        private eTurns m_CurrentTurn;
+        private eGameMode m_GameMode;
+        private eTurns m_CurrentTurn;
+        private char m_EmptySlotChar;
+
+        
+        // Ofir - maybe we should make some other constructors with different parameters
+        // to give the user more options - improve reusabillity
+        public ReverseTicTacToe(int i_BoardDimension, eGameMode i_GameMode)
+        {
+            //Ofir - since we didnt learn exceptions yet, how should be handle out of range board size in constructor?
+            if(i_BoardDimension < k_GameBoardMinimumDimension || i_BoardDimension > k_GameBoardMaximumDimension)
+                m_BoardDimension = k_defaultBoardDimension;
+            m_GameMode = i_GameMode;
+            m_Player1Char = k_defaultPlayer1Char;
+            m_Player2Char = k_defaultPlayer2Char;
+            currentTurn = k_DefaultFirstTurn;
+            m_GameBoard = new char[m_BoardDimension, m_BoardDimension];
+            m_EmptySlotChar = k_DefaultFirstTurn;
+
+            foreach(char currLocation in m_GameBoard)
             {
-                m_Player1 = new Player('X', false);
-                m_Player2 = new Player('O', false);
-            } else if(i_GameType == eGameTypes.PVC)
-            {
-                m_Player1 = new Player('X', false);
-                m_Player2 = new Player('O', true);
+                currLocation = ' ';
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
